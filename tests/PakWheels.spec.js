5d0229043aa54711132476b7a81ab8bea6bdbf9c
Hamza8381 <Hamzasaleem8381@gmail.com>
2024-10-31 08:10:45 -0700

    SignIn Successfully using Pakwheel Website

--- tests/PakWheels.spec.js
+++ tests/PakWheels.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 const Signup = require("../Pages/signUpPakWheelPO");
-
+const SignIn = require("../pages/SigninPakWheelPO");
 test.describe("Pakwheel Signup and SignIn using POM", () => {
   test("SignUp", async ({ page }) => {
     const signup = new Signup(page);
@@ -8,5 +8,13 @@ test.describe("Pakwheel Signup and SignIn using POM", () => {
     await signup.Signupnewuser();
     await signup.signupSuccessful();
     await page.waitForTimeout(4000);
+  });
+
+  test.only("signIn", async ({ page }) => {
+    const signin = new SignIn(page);
+    await signin.signInLink();
+    await signin.signInfun();
+    await page.pause();
+    await signin.signInSuccessful();
   });
 });
